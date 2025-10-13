9103d01899427d9cb31460106c2ec2a41595dbaa
Abdulahi338 <cabdulahii723@gmail.com>
2025-10-13 12:00:45 +0300

    adding exercise 1

---
+++ src/Exercise1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function exercise1() {
+  return (
+    <div>
+      <h1>Abdullahi Ibraim Osman</h1>
+      <p>cabdulahii723@gmail.com</p>
+    </div>
+  )
+}
+
+export default exercise1
